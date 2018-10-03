ca24b50ffb58ba70926ce4f5c1884ccc53e788b9
Austin Dean <adean31@uncc.edu>
2018-10-02 21:07:44 -0400

    Have a camera that 'works' and some player movement that 'works'. There are some animations in there, haven't gotten them to work yet though.

---
+++ Assets/scripts/Camera_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+    
+    public enum RotationAxis
+    {
+        RotMouseX = 1, 
+        RotMouseY = 2
+    }
+
+    public RotationAxis axis = RotationAxis.RotMouseX;
+
+    [SerializeField] private float minimumV;
+    private float maximumV = 70f;
+    private float sensitivityH = 10f;
+    private float sensitivityV = 10f;
+
+    public float rotationX = 0;
+    // Update is called once per frame
+    void Update() {
+
+        if (axis == RotationAxis.RotMouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityH, 0);
+        } else if (axis == RotationAxis.RotMouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityV;
+            rotationX = Mathf.Clamp(rotationX, minimumV, maximumV);
+
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+	}
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController characterCont;
+    private float maxSpeed;
+    private float gravity = -9.8f;
+
+    // Use this for initialization
+    void Start () {
+        characterCont = GetComponent<CharacterController>();
+        maxSpeed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float Xinput = Input.GetAxis("Horizontal") * maxSpeed;
+        float Zinput = Input.GetAxis("Vertical") * maxSpeed;
+
+        Vector3 velocity = new Vector3(Xinput, 0, Zinput);
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        velocity.y = gravity;
+
+        velocity *= Time.deltaTime;
+        velocity = transform.TransformDirection(velocity);
+        characterCont.Move(velocity);
+
+        
+    }
+}
