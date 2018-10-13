274489c14f2dda00cf1291cfe7718c2f1ff01598
Austin Dean <adean31@uncc.edu>
2018-10-13 00:38:15 -0400

    Changed PlayerMovement to PlayerController, added an attack animation and action. Still need to configure weapons to be able to swap them when the player is holding one. Need to work more with the player states to smooth out animations and have a good feeling character. Changed the camera to follow the players head, adding in headbob, and also following any animation we add later on.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,27 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     private float maxSpeed;
     private float gravity;
     [SerializeField] private Animator animations;
     [SerializeField] private CharacterController characterCont;
 
-    void Start () {
+    private bool isAttacking;
+
+    void Start()
+    {
         maxSpeed = 5f;
         gravity = -9.8f;
+        isAttacking = false;
     }
-	
-	void Update () {
 
+    void Update()
+    {
+            PlayerAttack();
+            PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
         //get the forward and sideways movement values
         float Xinput = Input.GetAxis("Horizontal") * maxSpeed;
         float Zinput = Input.GetAxis("Vertical") * maxSpeed;
 
         //sets velocity vector
         Vector3 velocity = new Vector3(Xinput, 0, Zinput);
+
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
         //keeps player on the ground
         velocity.y = gravity;
 
@@ -34,5 +47,19 @@ public class PlayerMovement : MonoBehaviour {
         velocity *= Time.deltaTime;
         velocity = transform.TransformDirection(velocity);
         characterCont.Move(velocity);
+
+    }
+
+    void PlayerAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isAttacking)
+        {
+            isAttacking = true;
+        }
+        else
+        {
+            isAttacking = false;
+        }
+        animations.SetBool("isAttacking", isAttacking);
     }
 }
