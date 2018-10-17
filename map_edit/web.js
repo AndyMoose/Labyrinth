9340a50138a3f09cd625913ad6a13de9d565e1e2
winlaptop2 <lmcgee12@uncc.edu>
2018-10-17 13:40:51 -0400

    reading file progress

--- map_edit/web.js
+++ map_edit/web.js
@@ -9,7 +9,11 @@ const scolorClass = "w3-red";
 var map = [];
 
 function createTable() {
+    var dvp = document.getElementById("main");
     var tbl = document.createElement("table")
+    //ondrop="dropHandler(event);"
+    //dvp.addEventListener("drop", dropHandler);
+    //dvp.addEventListener("drag", dragOverHandler);
     tbl.style.backgroundColor = ncolor;
     tbl.style.width = "100%";
     tbl.style.height = window.innerHeight + "px";
@@ -29,7 +33,9 @@ function createTable() {
         }
         tbl.appendChild(rw);
     }
-    document.body.appendChild(tbl);
+    //ondrop="dropHandler(event);
+    dvp.appendChild(tbl);
+    document.body.appendChild(dvp);
 }
 
 function addPlacement(div) {
@@ -105,6 +111,57 @@ function addPlacement(div) {
     return div;
 }
 
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+    
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+    var reader = new FileReader();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+            // If dropped items aren't files, reject them
+            if (ev.dataTransfer.items[i].kind === 'file') {
+                var file = ev.dataTransfer.items[i].getAsFile();
+                console.log('... file[' + i + '].name = ' + file.name);
+
+                reader.onload = function (e) {
+                    var text = reader.result;
+                    console.log(text);
+                }
+                reader.readAsText(file);
+            }
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+            console.log(ev.dataTransfer.files[i].getData("text/plain"));
+        }
+    }
+
+    // Pass event to removeDragData for cleanup
+    removeDragData(ev)
+}
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
+function removeDragData(ev) {
+    console.log('Removing drag data');
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to remove the drag data
+        ev.dataTransfer.items.clear();
+    } else {
+        // Use DataTransfer interface to remove the drag data
+        ev.dataTransfer.clearData();
+    }
+}
 
 
 createTable();
